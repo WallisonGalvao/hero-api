934301c214128973a3e93eade0afbe094ecb39ab
WallisonGalvao <wallison08@gmail.com>
2024-03-07 12:08:50 -0300

    chore: change de .net version (6 to 8) and add an constructor to DataContext for the database

--- HeroAPI/Data/DataContext.cs
+++ HeroAPI/Data/DataContext.cs
@@ -4,5 +4,12 @@ namespace HeroAPI.Data
 {
     public class DataContext : DbContext
     {
+        //Aqui faremos a injeção de contexto para que possamos acessar as informações dos heróis no banco de dados
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Hero> Heroes { get; set; }
+
+
     }
 }
